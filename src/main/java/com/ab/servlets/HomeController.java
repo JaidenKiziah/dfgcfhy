d3a3d47849ecd01ba59fbbaa229c6135034a15ba
Jaiden <jaidenmccarney@gmail.com>
2023-05-16 15:25:05 +0100

    web apps 1

---
+++ src/main/java/com/ab/servlets/HomeController.java
@@ -0,0 +1,51 @@
+package com.ab.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeController
+ */
+@WebServlet("/")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html"); //setting the type of the content that is going to be displayed
+		PrintWriter pw = response.getWriter();
+		pw.println("Hello world"); //writing out hello world
+	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		processRequest(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		processRequest(request, response);
+	}
+
+}
